6657699cf1140f0bc77ca64993bb63394ed318a2
keremersann <keremersan01@gmail.com>
2023-05-16 13:09:37 +0300

    Adding filter feature, which enables us to select the appropriate elements

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -4,19 +4,28 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args) {
-        printAllNumbersWithStructured(List.of(12,3,4,5,0,94));
+        //printAllNumbersWithFunctional(List.of(12,3,4,5,0,94));
+        printEvenNumbersWithFunctional(List.of(12,3,4,5,0,94));
     }
 
     public static void print(int number){
         System.out.println(number);
     }
 
-    private static void printAllNumbersWithStructured(List<Integer> integers) {
+    private static void printAllNumbersWithFunctional(List<Integer> integers) {
         // [12,3,4,5,0,94]
         // 12
         // 3
         // 4
-        // FP01Functional::print -> method reference
-        integers.stream().forEach(FP01Functional::print);
+        integers.stream().forEach(FP01Functional::print); // FP01Functional::print -> method reference
+    }
+    private static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+    private static void printEvenNumbersWithFunctional(List<Integer> integers) {
+
+        integers.stream()
+                .filter(FP01Functional::isEven) // filter -> define which elements should go further
+                .forEach(System.out::println);  // Method reference
     }
 }
--- src/programming/FP01Structured.java
+++ src/programming/FP01Structured.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 public class FP01Structured {
     public static void main(String[] args) {
-        printAllNumbersWithStructured(List.of(12,3,4,5,0,94));
+        //printAllNumbersWithStructured(List.of(12,3,4,5,0,94));
+        printEvenNumbersWithStructured(List.of(12,3,4,5,0,94));
     }
 
     private static void printAllNumbersWithStructured(List<Integer> integers) {
         for (Integer number: integers) {
             System.out.println(number);
+        }
+    }
+    private static void printEvenNumbersWithStructured(List<Integer> integers) {
+        for (Integer number: integers) {
+            if(number % 2 == 0)
+                System.out.println(number);
         }
     }
 }
